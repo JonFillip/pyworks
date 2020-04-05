08de4347a85439ecf682b6c3eda5f7d86d308b82
JonFillip <tjayphil14@gmail.com>
2020-04-05 21:00:06 +0300

    Regex test

---
+++ files_and_exceptions/test_regex.py
@@ -0,0 +1,23 @@
+import re
+
+
+def red_green_blue():
+    """This function is to a regex list from the file rgb.txt"""
+    filename = '/Users/johnphillip/Desktop/python_work/files_and_exceptions' \
+               '/rgb.txt'
+    regex_list = []
+    with open(filename, 'r+') as file_object:
+        reader = file_object.readlines()
+        file_object.seek(0)
+        for line in reader:
+            if line != 'red       green         blue       colorname\n':
+                file_object.write(line)
+        file_object.truncate()
+        n_reader = [re.sub(r'\s+', '\\t', line.strip()) for line in reader]
+        file_object.write(line)
+        file_object.truncate()
+        print(n_reader)
+
+
+if __name__ == "__main__":
+    red_green_blue()
